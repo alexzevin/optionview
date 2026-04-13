0a96ed8b1bbe2cae67322f018255fa33abeee262
alexzevin <19458368+alexzevin@users.noreply.github.com>
2026-04-12 21:29:53 -0500

    add compare module to public exports, bump version to 0.1.2

--- optionview/__init__.py
+++ optionview/__init__.py
@@ -1,10 +1,16 @@
 """optionview - Compare option pricing models against live market data."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from optionview.models import black_scholes, binomial_tree, monte_carlo, implied_volatility
 from optionview.greeks import compute_greeks
 from optionview.fetcher import fetch_option_chain, fetch_spot_price, fetch_dividend_yield
+from optionview.compare import (
+    compare_to_market,
+    ComparisonResult,
+    ComparisonReport,
+    SkippedRecord,
+)
 
 __all__ = [
     "black_scholes",
@@ -15,4 +21,8 @@ __all__ = [
     "fetch_option_chain",
     "fetch_spot_price",
     "fetch_dividend_yield",
+    "compare_to_market",
+    "ComparisonResult",
+    "ComparisonReport",
+    "SkippedRecord",
 ]
